39e0b235de1dd77ee24a0a82ad2dd6c71ee26e64
dtan4 <dtanshi45@gmail.com>
2014-07-21 23:42:41 +0900

    Include DeployGate SDK

---
+++ app/src/main/java/org/dtan4/farecolle/App.java
@@ -0,0 +1,12 @@
+package org.dtan4.farecolle;
+
+import android.app.Application;
+import com.deploygate.sdk.DeployGate;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DeployGate.install(this);
+    }
+}
