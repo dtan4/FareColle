c97a507043bbeecf03d7279bf970d70d100819fb
dtan4 <dtanshi45@gmail.com>
2014-07-22 14:22:18 +0900

    Extract FelicaReader class

--- app/src/main/java/org/dtan4/farecolle/MainActivity.java
+++ app/src/main/java/org/dtan4/farecolle/MainActivity.java
@@ -19,6 +19,8 @@ import android.os.Build;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.dtan4.farecolle.util.FelicaReader;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
@@ -77,29 +79,11 @@ public class MainActivity extends Activity {
                 return;
             }
 
-            byte[] idm = tag.getId();
-            String idStr = toHex(idm);
-
+            FelicaReader reader = new FelicaReader(tag);
             TextView tv = (TextView)findViewById(R.id.cardIdView);
-            tv.setText(idStr);
+            tv.setText(reader.felicaIDStr());
 
-            NfcF nfc = NfcF.get(tag);
-
-            try {
-                nfc.connect();
-
-                try {
-                    byte[] req = readWithoutEncryption(idm, 10);
-                    Log.d(TAG, "req: " + toHex(req));
-                    byte[] res = nfc.transceive(req);
-                    Log.d(TAG, "res: " + toHex(res));
-                } finally {
-                    nfc.close();
-                }
-            } catch (Exception e) {
-                Log.d(TAG, e.getMessage());
-                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
+            reader.getHistory();
         }
     }
 
@@ -139,42 +123,6 @@ public class MainActivity extends Activity {
         PendingIntent pendingIntent = PendingIntent.getActivity(
                 getApplicationContext(), 0, intent, 0);
         nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
-    }
-
-    private byte[] readWithoutEncryption(byte[] idm, int size) throws IOException {
-        ByteArrayOutputStream bout = new ByteArrayOutputStream(100);
-
-        bout.write(0);           // データ長バイトのダミー
-        bout.write(0x06);        // Felicaコマンド「Read Without Encryption」
-        bout.write(idm);         // カードID 8byte
-        bout.write(1);           // サービスコードリストの長さ(以下２バイトがこの数分繰り返す)
-        bout.write(0x0f);        // 履歴のサービスコード下位バイト
-        bout.write(0x09);        // 履歴のサービスコード上位バイト
-        bout.write(size);        // ブロック数
-
-        for (int i = 0; i < size; i++) {
-            bout.write(0x80);    // ブロックエレメント上位バイト 「Felicaユーザマニュアル抜粋」の4.3項参照
-            bout.write(i);       // ブロック番号
-        }
-
-        byte[] msg = bout.toByteArray();
-        msg[0] = (byte) msg.length; // 先頭１バイトはデータ長
-
-        return msg;
-    }
-
-    private String toHex(byte[] bytes) {
-        StringBuilder sb = new StringBuilder(bytes.length * 2);
-
-        for (byte b : bytes) {
-            String hex = Integer.toHexString(b & 0xff);
-            if (hex.length() == 1) {
-                sb.append("0");
-            }
-            sb.append(hex);
-        }
-
-        return sb.toString();
     }
 
     /**
---
+++ app/src/main/java/org/dtan4/farecolle/util/FelicaReader.java
@@ -0,0 +1,73 @@
+package org.dtan4.farecolle.util;
+
+import android.nfc.Tag;
+import android.nfc.tech.NfcF;
+import android.util.Log;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class FelicaReader {
+    private static final String TAG = "farecolle.util.FelicaReader";
+    private NfcF nfc;
+    private byte[] felicaIDm;
+
+    public FelicaReader(Tag tag) {
+        nfc = NfcF.get(tag);
+        felicaIDm = tag.getId();
+    }
+
+    public String felicaIDStr() {
+        return toHex(felicaIDm);
+    }
+
+    public void getHistory() {
+        try {
+            nfc.connect();
+
+            try {
+                byte[] request = readWithoutEncryption(felicaIDm, 10);
+                byte[] response = nfc.transceive(request);
+            } finally {
+                nfc.close();
+            }
+        } catch (Exception e) {
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    private byte[] readWithoutEncryption(byte[] idm, int size) throws IOException {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream(100);
+
+        bout.write(0);           // データ長バイトのダミー
+        bout.write(0x06);        // Felicaコマンド「Read Without Encryption」
+        bout.write(idm);         // カードID 8byte
+        bout.write(1);           // サービスコードリストの長さ(以下２バイトがこの数分繰り返す)
+        bout.write(0x0f);        // 履歴のサービスコード下位バイト
+        bout.write(0x09);        // 履歴のサービスコード上位バイト
+        bout.write(size);        // ブロック数
+
+        for (int i = 0; i < size; i++) {
+            bout.write(0x80);    // ブロックエレメント上位バイト 「Felicaユーザマニュアル抜粋」の4.3項参照
+            bout.write(i);       // ブロック番号
+        }
+
+        byte[] msg = bout.toByteArray();
+        msg[0] = (byte) msg.length; // 先頭１バイトはデータ長
+
+        return msg;
+    }
+
+    private String toHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length * 2);
+
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(b & 0xff);
+            if (hex.length() == 1) {
+                sb.append("0");
+            }
+            sb.append(hex);
+        }
+
+        return sb.toString();
+    }
+}
